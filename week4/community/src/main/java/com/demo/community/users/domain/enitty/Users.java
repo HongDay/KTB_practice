1e3a19e1fcc3f273b61e98a1c966440ba69b5061
Day HONG <tot032874@gmail.com>
2025-10-11 23:38:53 +0900

    base entity implemented

--- week4/community/src/main/java/com/demo/community/users/domain/enitty/Users.java
+++ week4/community/src/main/java/com/demo/community/users/domain/enitty/Users.java
@@ -1,21 +1,31 @@
 package com.demo.community.users.domain.enitty;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import com.demo.community.common.domain.entity.BaseEntity;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
 @Getter
 @Setter
-@NoArgsConstructor
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
-public class Users {
-    @Id
+public class Users extends BaseEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(unique = true, nullable = false)
     private String email;
+
+    // SHA-256 해싱 알고리즘 사용
+    @Column(length = 64, nullable = false)
     private String password;
-    private String nickname;
+
+    @Column (length = 20, nullable = false)
+    private String nickname = null;
+
+    @Column (nullable = false)
+    @Builder.Default
+    private String profileImage = "https://i.namu.wiki/i/M0j6sykCciGaZJ8yW0CMumUigNAFS8Z-dJA9h_GKYSmqqYSQyqJq8D8xSg3qAz2htlsPQfyHZZMmAbPV-Ml9UA.webp";
 }
